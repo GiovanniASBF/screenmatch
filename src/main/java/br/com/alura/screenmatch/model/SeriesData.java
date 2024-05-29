db9a8d08c148caa123d5a84d6ad7d84811668ea4
GiovanniBF <giovannibernardesfigueiredo@gmail.com>
2024-05-29 15:59:40 -0300

    Dependency of Jackson included in the project and mapping of the response json to SeriesData class

---
+++ src/main/java/br/com/alura/screenmatch/model/SeriesData.java
@@ -0,0 +1,8 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record SeriesData(@JsonAlias("Title") String title,
+                         @JsonAlias("totalSeasons") Integer totalSeasons,
+                         @JsonAlias("imdbRating") String rating) {
+}
