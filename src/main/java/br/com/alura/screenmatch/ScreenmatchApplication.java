fc7bf243095ae6ab0d7cf9aac47bad77ce9accd9
GiovanniBF <giovannibernardesfigueiredo@gmail.com>
2024-05-29 08:33:26 -0300

    Service package and class to realize consumption of apis created.

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.service.ApiConsumption;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Projeto Spring sem web.");
+		ApiConsumption apiConsumption = new ApiConsumption();
+		var json = apiConsumption.obtainData("http://www.omdbapi.com/?t=house+of+the+dragon&Season=1&apikey=e4335e62");
+		System.out.println(json);
 	}
 }
