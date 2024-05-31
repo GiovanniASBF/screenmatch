4acbe74c8672d991c97e69517b77877035214abf
GiovanniBF <giovannibernardesfigueiredo@gmail.com>
2024-05-31 11:15:02 -0300

    Data conversion interface created, along with implementation through a new service with the same name. Added JsonIgnoreProperties to specify the converted data.

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.SeriesData;
 import br.com.alura.screenmatch.service.ApiConsumption;
+import br.com.alura.screenmatch.service.DataConvert;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +17,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		ApiConsumption apiConsumption = new ApiConsumption();
-		var json = apiConsumption.obtainData("http://www.omdbapi.com/?t=house+of+the+dragon&Season=1&apikey=e4335e62");
+		var json = apiConsumption.obtainData("http://www.omdbapi.com/?t=house+of+the+dragon&apikey=e4335e62");
 		System.out.println(json);
+
+		DataConvert converter = new DataConvert();
+		SeriesData data = converter.getData(json, SeriesData.class);
+		System.out.println(data);
 	}
 }
