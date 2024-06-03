3bc624643e5a4e5be3c45b276b4a41cfd7d7a8e1
GiovanniBF <giovannibernardesfigueiredo@gmail.com>
2024-06-03 15:05:52 -0300

    Complete decoupling of the main method.

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -2,9 +2,7 @@ package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.model.EpisodeData;
 import br.com.alura.screenmatch.model.SeasonData;
-import br.com.alura.screenmatch.model.SeriesData;
-import br.com.alura.screenmatch.service.ApiConsumption;
-import br.com.alura.screenmatch.service.DataConvert;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,27 +19,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ApiConsumption apiConsumption = new ApiConsumption();
-		var json = apiConsumption.obtainData("http://www.omdbapi.com/?t=house+of+the+dragon&apikey=e4335e62");
-		System.out.println(json);
-
-		DataConvert converter = new DataConvert();
-		//Delivering serie data
-		SeriesData data = converter.getData(json, SeriesData.class);
-		System.out.println(data);
-
-		//Delivering episode data
-		json = apiConsumption.obtainData("http://www.omdbapi.com/?t=house+of+the+dragon&season=1&episode=4&apikey=e4335e62");
-		EpisodeData episodeData = converter.getData(json, EpisodeData.class);
-		System.out.println(episodeData);
-
-		//Delivering all seasons data
-		List<SeasonData> seasons = new ArrayList<>();
-		for (int i = 1; i <= data.totalSeasons() ; i++) {
-			json = apiConsumption.obtainData("http://www.omdbapi.com/?t=house+of+the+dragon&season=" + i +"&apikey=e4335e62");
-			SeasonData seasonData = converter.getData(json, SeasonData.class);
-			seasons.add(seasonData);
-		}
-		seasons.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.showMenu();
 	}
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,19 +1,40 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.EpisodeData;
+import br.com.alura.screenmatch.model.SeasonData;
+import br.com.alura.screenmatch.model.SeriesData;
 import br.com.alura.screenmatch.service.ApiConsumption;
+import br.com.alura.screenmatch.service.DataConvert;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner scan = new Scanner(System.in);
+    private ApiConsumption apiConsumption = new ApiConsumption();
+    private DataConvert converter = new DataConvert();
+
     private final String ADRESS = "http://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=e4335e62";
-    private ApiConsumption apiConsumption = new ApiConsumption();
+
     public void showMenu(){
         System.out.println("Enter the series name for the search: ");
         var serieName = scan.nextLine();
-
         var json = apiConsumption.obtainData(ADRESS + serieName.replaceAll(" ", "+") + API_KEY);
+
+        //Delivering serie data
+        SeriesData data = converter.getData(json, SeriesData.class);
+        System.out.println(data);
+
+        //Delivering all seasons data
+        List<SeasonData> seasons = new ArrayList<>();
+        for (int i = 1; i <= data.totalSeasons() ; i++) {
+            json = apiConsumption.obtainData(ADRESS + serieName.replaceAll(" ", "+") + "&season=" + i + API_KEY);
+            SeasonData seasonData = converter.getData(json, SeasonData.class);
+            seasons.add(seasonData);
+        }
+        seasons.forEach(System.out::println);
     }
 }
